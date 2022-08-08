c8b7bbd0b60595bd23d46143ed5d7b7e7892a91d
smh0505 <42821865+smh0505@users.noreply.github.com>
2022-08-08 15:47:37 -0400

    Updated v.1.1

--- main.py
+++ main.py
@@ -1,352 +1,113 @@
-from tkinter import Canvas, Tk, ttk, IntVar, messagebox
+import tkinter as tk
+from tkinter import Frame, ttk
+import pandas as pd
 
-from numpy import expand_dims, true_divide
-
-class Window(Tk):
+# Base Window (Object Oriented)
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
 
         # Window Initialization
         self.title('리듬 끝말잇기')
-        self.geometry('250x780')
-        self.resizable(0, 0)
+        self.geometry('600x600')
+        self.resizable(False, False)
 
-        # Form Initialization
-        self.font_init()
-        self.mode_init()
-        ttk.Separator(self, orient='horizontal').pack(fill='x', padx=5, side='top')
-        self.nextSong_init()
-        ttk.Separator(self, orient='horizontal').pack(fill='x', padx=5, side='top')
-        ttk.Label(self, text='절취선').pack(fill='y', pady=5)
-        ttk.Separator(self, orient='horizontal').pack(fill='x', padx=5, side='top')
-        self.rem_init()
-        ttk.Separator(self, orient='horizontal').pack(fill='x', padx=5, side='top')
-        self.currSong_init()
-        ttk.Separator(self, orient='horizontal').pack(fill='x', padx=5, side='top')
-        self.songlist_init()
+        # Widget Initialization
+        self.init_font()
+        self.read_songlist()
+        self.init_songlist()
+        self.reset_songlist()
+        self.init_search()
 
+    # List of fonts
+    def init_font(self):
+        self.font1 = ('여기어때 잘난체', 24)
+        self.font2 = ('강원교육모두 Bold', 16)
 
-        # Variables Initialization
-        self.timerBool = False
-        self.marqBool = False
-        self.marqBool2 = False
-        self.startBool = False
-        self.songcount = 0
-        self.songlist = '플레이한 목록\n'
-
-    # List of fonts    
-    def font_init(self):
-        self.font1 = ('강원교육모두 Bold', 16)
-        self.font2 = ('강원교육모두 Bold', 28)
-        self.font3 = ('Kotra Leap', 32)
-        self.font4 = ('Kotra Leap', 20)
-        self.font5 = ('Kotra Leap', 40)
-        self.font6 = ('Kotra Leap', 24)
-        self.font7 = ('Kotra Leap', 16)
-
-    # Gamemode
-    def mode_init(self):
-        # Frame
-        up = ttk.Frame(self)
-        up.columnconfigure(0, minsize=70)
-        up.columnconfigure(1, weight=1)
-        up1 = ttk.Frame(up)
+    # Song list
+    def read_songlist(self):
+        songlist = pd.read_excel('list.xlsx').values.tolist()
+        self.baseSL = []
         
-        # Radiobuttons
-        self.game_mode = IntVar(value=0)
-        self.num_songs = IntVar(value=1)
-        self.rb_1 = ttk.Radiobutton(up, text='개인전', value=0, variable=self.game_mode, command=self.change_mode)
-        self.rb_2 = ttk.Radiobutton(up, text='팀전', value=1, variable=self.game_mode, command=self.change_mode)
+        for item in songlist:
+            if '있' in item[7] and item[3] * 60 + item[4] <= 180:
+                self.baseSL.append((item[0], item[1], item[2],
+                    '{}:{:02}'.format(item[3], item[4]), item[5], item[6],
+                    item[8], item[9]))
 
-        # Spinbar
-        self.sb = ttk.Spinbox(up1, from_=1, to=50, textvariable=self.num_songs, wrap=True, state='disabled', command=self.change_num)
+    def init_songlist(self):
+        # Widget Wrapper
+        songlist_group = Frame(self)
 
-        # Start Button
-        self.start_button = ttk.Button(up, text='출근', command=self.start_game)
-        
-        # Placing
-        self.start_button.grid(column=2, row=0, rowspan=3, sticky='news')
+        # Song list
+        self.table = ttk.Treeview(
+            songlist_group,
+            columns=('title', 'search', 'composer', 'time',
+                'game', 'section', 'start', 'end'),
+            displaycolumns=('title', 'time', 'game'),
+            show='headings',
+            selectmode='browse'
+        )
+        self.table.column('time', width=40, stretch=0)
+        self.table.column('game', width=120, stretch=0)
+        self.table.heading('title', text='곡명', anchor='w')
+        self.table.heading('time', text='시간', anchor='w')
+        self.table.heading('game', text='수록 게임', anchor='w')
 
-        ttk.Label(up, text='현재 설정').grid(column=0, row=0, columnspan=2, sticky='news')
-        self.rb_1.grid(column=0, row=1, columnspan=2, sticky='news')
-        self.rb_2.grid(column=0, row=2, sticky='nwes')
-
-        ttk.Label(up1, text='곡').pack(fill='both', side='right', expand=1)
-        self.sb.pack(fill='both', side='left', expand=1)
-        up1.grid(column=1, row=2, sticky='nwes', padx=(0, 5))
-        
-        up.pack(fill='both', side='top', padx=5, pady=5)
-        
-    # Remaining
-    def rem_init(self):
-        # Frame
-        downFrame = ttk.Frame(self)
-        downFrame.columnconfigure(0, weight=1)
-
-        # Labels
-        self.label3 = ttk.Label(downFrame, font=self.font1, text='퇴근까지 걸린 시간')
-        self.label4 = ttk.Label(downFrame, font=self.font2, text='00:00:00')
+        # Scrollbar
+        scroll = ttk.Scrollbar(
+            songlist_group, 
+            orient=tk.VERTICAL, 
+            command=self.table.yview
+        )
+        self.table.configure(yscrollcommand=scroll.set)
 
         # Placing
-        self.label3.grid(column=0, row=0)
-        self.label4.grid(column=0, row=1)
-        downFrame.pack(fill='both', side='top', pady=10)
-
-    # Next Song
-    def nextSong_init(self):
-        # Frame
-        mid = ttk.Frame(self)
-        mid.columnconfigure(0, minsize=90)
-        mid.columnconfigure(1, weight=1)
-        mid1 = ttk.Frame(mid)
-
-        # Variables
-        self.chk_var = IntVar(value=0)
-        initials = [
-            'A', 'B', 'C', 'D', 'E', 'F',
-            'G', 'H', 'I', 'J', 'K', 'L',
-            'M', 'N', 'O', 'P', 'Q', 'R',
-            'S', 'T', 'U', 'V', 'W', 'X',
-            'Y', 'Z', '0', '1', '2', '3',
-            '4', '5', '6', '7', '8', '9'
-        ]
-
-        # Labels (column 0)
-        ttk.Label(mid, text='곡 제목').grid(column=0, row=0, sticky='news', pady=(0, 5))
-        ttk.Label(mid, text='다음 이니셜').grid(column=0, row=1, sticky='news', pady=(0, 5))
-        ttk.Label(mid, text='추가 이니셜').grid(column=0, row=2, sticky='news', pady=(0, 5))
-        
-        # Inputs (column 1)
-        self.input = ttk.Entry(mid, state='disabled')
-        self.cb_1 = ttk.Combobox(mid, values=initials, state='disabled')
-        self.cb_1.current(0)
-        self.chk = ttk.Checkbutton(mid1, variable=self.chk_var, state='disabled', command=self.additional)
-        self.cb_2 = ttk.Combobox(mid1, values=initials, state='disabled')
-        self.cb_2.current(0)
-
-        self.input.grid(column=1, row=0, sticky='news', pady=(0, 5))
-        self.cb_1.grid(column=1, row=1, sticky='news', pady=(0, 5))
-        self.chk.pack(side='left')
-        self.cb_2.pack(fill='x', side='right', expand=1)
-        mid1.grid(column=1, row=2, sticky='nwes', pady=(0, 5))
-
-        # Buttons
-        self.add_button = ttk.Button(mid, text='곡 추가', state='disabled', command=self.add_song)
-        self.add_button.grid(column=1, row=3, sticky='news')
-
-        mid.pack(fill='both', side='top', padx=5, pady=5)
-
-    # Current Song (Marquee)
-    def currSong_init(self):
-        self.can1 = Canvas(self, width=240, height=150)
-        self.can1.pack(side='top', pady=2)
-        
-        self.can1.create_text(122, 72, text='리듬\n끝말잇기', font=self.font3, justify='center', fill='black', tags=('sn'))
-        self.can1.create_text(120, 300, text='다음 알파벳', font=self.font4, fill='black', tags=('txt'))
-        self.can1.create_text(120, 300, text='A/A', font=self.font5, fill='orange', tags=('ni'))
-
-    # Songlist (Marquee)
-    def songlist_init(self):
-        self.can2 = Canvas(self, width=240, height=300)
-        self.can2.pack(side='top', pady=2)
-
-        self.can2.create_text(122, 140, text='아직\n곡이\n없습니다!', font=self.font5, justify='center', fill='black', tags=('sl'))
-
-    # Changing Gamemode
-    def change_mode(self):
-        mode = self.game_mode.get()
-        if mode == 0:
-            self.label3['text'] = '퇴근까지 걸린 시간'
-            self.label4['text'] = '00:00:00'
-            self.sb['state'] = 'disabled'
-        if mode == 1:
-            self.label3['text'] = '레진 정산까지'
-            self.label4['text'] = '{}곡 남음'.format(self.num_songs.get())
-            self.sb['state'] = 'readonly'
-
-    # Changing Number of Songs to Play
-    def change_num(self):
-        self.label4['text'] = '{}곡 남음'.format(self.num_songs.get())
+        self.table.pack(side='left', expand=1, fill='x')
+        scroll.pack(side='left', fill='y')
+        songlist_group.pack(side='top', padx=10, pady=10, fill='x')
     
-    # Start Button Function
-    def start_game(self):
-        mode = self.game_mode.get()
+    def reset_songlist(self):
+        self.currentSL = self.baseSL
+        self.table.delete(*self.table.get_children())
+        for item in self.currentSL:
+            self.table.insert('', tk.END, values=item)
+        #self.update()
 
-        if self.start_button['text'] == '출근':     # Start Game
-            # Gamemode
-            self.rb_1['state'] = 'disabled'
-            self.rb_2['state'] = 'disabled'
-            self.sb['state'] = 'disabled'
+    def init_search(self):
+        # Widget Wrapper
+        search_group = Frame(self)
+        cb_group = Frame(search_group)
 
-            # Next Song
-            self.input['state'] = 'normal'
-            self.input.focus()
-            self.cb_1['state'] = 'readonly'
-            self.chk['state'] = 'normal'
-            self.add_button['state'] = 'normal'
+        # Game list
+        self.gamelist = ttk.Combobox(
+            cb_group, 
+            state='readonly'
+        )
+        games = []
+        for item in self.baseSL:
+            if item[4] not in games:
+                games.append(item[4])
+        games.insert(0, '전체 게임')
+        self.gamelist['values'] = games
+        self.gamelist.current(0)
 
-            # Current Song
-            self.can1.itemconfig('sn', text='첫 곡을\n플레이 해주세요!', font=self.font6)
+        # Section list
+        self.sectionlist = ttk.Combobox(
+            cb_group,
+            state='disable'
+        )
 
-            if mode == 0:   # Free For All (FFA)
-                self.timerBool = True
-                self.timer(0)
-                self.start_button['text'] = '퇴근'
-            
-            if mode == 1:   # Team Match (TM)
-                self.start_button['text'] = '리셋'
-                self.start_button['state'] = 'disabled'
-                self.songcount = self.num_songs.get()
-        
-        elif self.start_button['text'] == '퇴근':   # End Game (FFA only)
-            if not self.startBool:
-                messagebox.showerror('리듬 끝말잇기', '아직 첫 곡을 플레이하지 않았습니다.\n첫 곡을 플레이 해주세요.')
-            else:
-                # Remaining
-                self.timerBool = False
+        # Search Bar
+        self.searchbar = ttk.Entry(search_group)
 
-                # Gamemode
-                self.start_button['text'] = '리셋'
+        # Placing
+        self.gamelist.pack(side='top')
+        self.sectionlist.pack(side='bottom', pady=(10, 0))
+        cb_group.pack(side='left')
+        self.searchbar.pack(side='left', expand=1, fill='both', padx=(10, 0))
+        search_group.pack(side='top', fill='x', padx=10)        
 
-                # Next Song
-                self.input.delete(0, 'end')
-                self.cb_1.current(0)
-                self.cb_2.current(0)
-                self.chk_var.set(0)
-                self.input['state'] = 'disabled'
-                self.cb_1['state'] = 'disabled'
-                self.cb_2['state'] = 'disabled'
-                self.chk['state'] = 'disabled'
-                self.add_button['state'] = 'disabled'
-
-                # Current Song
-                self.can1.itemconfig('ni', text='끝')
-
-        elif self.start_button['text'] == '리셋':   # Reset Game
-            # Gamemode
-            self.start_button['text'] = '출근'
-            self.rb_1['state'] = 'normal'
-            self.rb_2['state'] = 'normal'
-            
-            # Remaining
-            if mode == 0:   # FFA
-                self.label4['text'] = '00:00:00'
-            if mode == 1:   # TM
-                self.sb['state'] = 'readonly'
-                self.label4['text'] = '{}곡 남음'.format(self.num_songs.get())
-            
-            # Current Song
-            self.can1.itemconfig('sn', text='리듬\n끝말잇기', font=self.font3)
-            self.can1.coords('sn', 122, 72)
-            self.can1.coords('txt', 120, 300)
-            self.can1.coords('ni', 120, 300)
-            self.marqBool = False
-
-            # Songlist
-            self.marqBool2 = False
-            self.can2.itemconfig('sl', text='아직\n곡이\n없습니다!', font=self.font5)
-            self.can2.coords('sl', 122, 140)
-            self.songlist = '플레이한 목록\n'
-
-    # Timer For FFA
-    def timer(self, t):
-        if self.timerBool:
-            hrs = t // 3600
-            mins = (t % 3600) // 60
-            secs = t % 60
-
-            self.label4['text'] = '{:02}:{:02}:{:02}'.format(hrs, mins, secs)
-            self.after(1000, self.timer, t + 1)
-
-    # Additional Initial
-    def additional(self):
-        mode = self.chk_var.get()
-        if mode == 0:
-            self.cb_2['state'] = 'disabled'
-        if mode == 1:
-            self.cb_2['state'] = 'readonly'
-
-    # Adding a Song to the Songlist
-    def add_song(self):
-        # Input
-        songName = self.input.get()
-        alpha = self.cb_1.get()
-        if self.chk_var.get() == 1:
-            alpha = alpha + '/' + self.cb_2.get()
-
-        # Reset Input
-        self.input.delete(0, 'end')
-        self.input.focus()
-        self.cb_1.current(0)
-        self.cb_2.current(0)
-        self.chk_var.set(0)
-        self.cb_2['state'] = 'disabled'
-        
-        # Output
-        if songName and not songName.isspace():
-            # Canvas
-            self.can1.itemconfig('sn', text=songName)
-            self.can1.itemconfig('ni', text=alpha)
-            self.can1.coords('txt', 70, 130)
-            self.can1.coords('ni', 190, 120)
-            
-            # Marquee
-            pos = self.can1.bbox('sn')
-            if pos[2] - pos[0] >= 230 and not self.marqBool:
-                self.can1.coords('sn', 10 + (pos[2] - pos[0]) // 2, 50)
-                self.marqBool = True
-                self.marquee1()
-            elif pos[2] - pos[0] < 230:
-                self.can1.coords('sn', 122, 50)
-                self.marqBool = False
-
-            # Song List
-            self.startBool = True
-            self.songlist = self.songlist + songName + '\n'
-            self.can2.itemconfig('sl', text=self.songlist, font=self.font7)
-            pos2 = self.can2.bbox('sl')
-            if pos2[3] - pos[1] >= 290 and not self.marqBool2:
-                self.marqBool2 = True
-                self.marquee2()
-            else: 
-                self.can2.coords('sl', 122, 10 + (pos2[3] - pos2[1]) // 2)
-
-            # For TM
-            if self.game_mode.get() == 1:
-                self.songcount -= 1
-                self.label4['text'] = '{}곡 남음'.format(self.songcount)
-
-                # No More Song
-                if self.songcount == 0:
-                    self.can1.itemconfig('ni', text='끝')
-                    self.input['state'] = 'disabled'
-                    self.cb_1['state'] = 'disabled'
-                    self.cb_2['state'] = 'disabled'
-                    self.chk['state'] = 'disabled'
-                    self.start_button['state'] = 'normal'
-                    self.add_button['state'] = 'disabled'
-        else:   # Empty Input
-            messagebox.showinfo('리듬 끝말잇기', '곡 제목 칸이 비어있습니다\n다시 적어주세요')
-
-    # Marquee Effect 1
-    def marquee1(self):
-        if self.marqBool:
-            pos = self.can1.bbox('sn')
-            if pos[2] < 0:
-                self.can1.coords('sn', 240 + (pos[2] - pos[0]) // 2, 50)
-            else:
-                self.can1.move('sn', -2, 0)
-            self.after(1000 // 30, self.marquee1)
-
-    def marquee2(self):
-        if self.marqBool2:
-            pos = self.can2.bbox('sl')
-            if pos[3] < 0:
-                self.can2.coords('sl', 122, 300 + (pos[3] - pos[1]) // 2)
-            else:
-                self.can2.move('sl', 0, -1)
-            self.after(1000 // 45, self.marquee2)
-
-if __name__ == '__main__':
-    app = Window()
+if __name__ == "__main__":
+    app = App()
     app.mainloop()
